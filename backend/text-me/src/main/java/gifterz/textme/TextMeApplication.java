7249c985757665c2423ef52cf00e2f29c3a63e3c
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-11-26 23:13:45 +0900

    feat: initialize

---
+++ backend/text-me/src/main/java/gifterz/textme/TextMeApplication.java
@@ -0,0 +1,13 @@
+package gifterz.textme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextMeApplication.class, args);
+	}
+
+}
