9817514cfceabdcda4020f8757bf6d199fc5dc22
chchaeun <kce3615@naver.com>
2022-11-30 15:38:59 +0900

    feat: 비로그인 사용자 전환 버튼 추가 (#2)

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useForm } from "react-hook-form";
 import { useRoomEnter } from "../stores/useRoomEnter";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 type EmailForm = {
   email: string;
 };
@@ -30,22 +31,26 @@ export default function Home() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onValid)}>
-      <label>이메일로 다른 사람 방 입장하기</label>
-      <input
-        {...register("email", {
-          required: "값을 입력해주세요.",
-          pattern: {
-            value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-            message: "올바른 이메일 형식이 아닙니다.",
-          },
-        })}
-        type="text"
-      />
-      {errors?.email && <em>{errors.email.message}</em>}
-      <button disabled={isLoading} type="submit">
-        입장
-      </button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit(onValid)}>
+        <label>이메일로 다른 사람 방 입장하기</label>
+        <input
+          {...register("email", {
+            required: "값을 입력해주세요.",
+            pattern: {
+              value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+              message: "올바른 이메일 형식이 아닙니다.",
+            },
+          })}
+          type="text"
+        />
+        {errors?.email && <em>{errors.email.message}</em>}
+        <button disabled={isLoading} type="submit">
+          입장
+        </button>
+      </form>
+      <Link href={`/signup`}>내 방 만들기</Link>
+      <Link href={`/signin`}>로그인</Link>
+    </div>
   );
 }
