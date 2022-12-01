be6e6dc3ce162dfc9c74ae7b9fc2559aa2d18215
juy4556 <kws05050@naver.com>
2022-12-01 11:17:07 +0900

    feat: 비밀번호 엔티티 추가(#8)

---
+++ backend/text-me/src/main/java/domain/member/entity/Password.java
@@ -0,0 +1,49 @@
+package domain.member.entity;
+
+import domain.member.exception.InvalidPasswordException;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.regex.Pattern;
+
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Password {
+    private String password;
+
+    public Password(String password) {
+        validate(password);
+        this.password = password;
+    }
+
+    private static void validate(String password) {
+        String REGEX = "^[a-z0-9]{10,20}$";
+        checkSize(password);
+        checkArgument(Pattern.matches(REGEX, password));
+    }
+
+    private static void checkSize(String password) {
+        if (password.isBlank() || password.length() < 8 || password.length() > 12) {
+            throw new IllegalArgumentException("Invalid Password");
+        }
+    }
+
+    private static void checkArgument(boolean matches) {
+        if (!matches) {
+            throw new IllegalArgumentException("Invalid Password");
+        }
+    }
+
+    public void checkSamePassword(String password) {
+        if (!this.password.equals(password)) {
+            throw new InvalidPasswordException("Incorrect Password");
+        }
+    }
+
+    public void setPassword(String encodedPassword) {
+        this.password = encodedPassword;
+    }
+}
