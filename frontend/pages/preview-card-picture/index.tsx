20ea6533c714c6188d7df837350c6f927baa3f0d
seungki <sysh9498@gmail.com>
2022-12-02 14:22:59 +0900

    feat: zustand로 picture URL설정

--- frontend/pages/preview-card-picture/index.tsx
+++ frontend/pages/preview-card-picture/index.tsx
@@ -1,13 +1,17 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import Router from 'next/router';
 import { useCardPicture } from '../../stores/useCardPicture';
+import { useRouter } from 'next/navigation';
 
 export default function index() {
+  const router = useRouter();
   const { pictureURL } = useCardPicture();
   return (
     <>
       <h1>사진 확인하기</h1>
+      <button onClick={() => router.back()}>뒤로가기</button>
       <img src={pictureURL.toString()} style={{ width: 100, height: 100 }} />
+      <button>편지쓰기</button>
     </>
   );
 }
--- frontend/pages/select-card-picture/index.tsx
+++ frontend/pages/select-card-picture/index.tsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Router from 'next/router';
-import Link from 'next/link';
 import { useCardPicture } from '../../stores/useCardPicture';
 const colors = ['red', 'orange', 'white', 'gray'];
 
@@ -9,7 +8,6 @@ export default function index() {
   const { pictureURL, setPictureURL } = useCardPicture();
   const router = useRouter();
   const [selectedPicture, setSelectedPicture] = useState<File | null>();
-  const [previewImage, setPreviewImage] = useState<string>('');
   const fileRef = useRef<HTMLInputElement>(null);
   const handleClick = () => {
     fileRef?.current?.click();
@@ -18,23 +16,12 @@ export default function index() {
   const fileUploadHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const target = e.currentTarget;
     setSelectedPicture(target.files[0]);
-    setPreviewImage(URL.createObjectURL(target.files[0]));
     console.log('URL:' + URL.createObjectURL(target.files[0]));
     setPictureURL(URL.createObjectURL(target.files[0]));
-    console.log(selectedPicture);
-    console.log(target.files[0]);
-
-    console.log(fileRef);
-
-    // if (files === undefined) {
-    //   return;
-    // }
-
-    // setSelectedPicture(files);
   };
 
   const sendFile = () => {
-    router.push(`/preview-card-picture?file=${previewImage}`);
+    router.push('/preview-card-picture');
   };
 
   return (
@@ -50,7 +37,7 @@ export default function index() {
           {colors.map(color => (
             <div key={color} style={{ width: 100, height: 100, backgroundColor: `${color}` }}></div>
           ))}
-          {previewImage && <img src={previewImage} style={{ width: 100, height: 100 }}></img>}
+          {pictureURL && <img src={pictureURL.toString()} style={{ width: 100, height: 100 }}></img>}
           <div onClick={sendFile}>버튼으로 전송하기</div>
         </form>
       </div>
