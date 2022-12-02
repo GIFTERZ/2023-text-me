4ca47750ca3e2ac640813e04f68b88e434de76fc
seungki <sysh9498@gmail.com>
2022-12-03 00:36:26 +0900

    feat: 캘린더, 코멘트 입력 창 추가(#16)

--- frontend/pages/preview-card-picture/index.tsx
+++ frontend/pages/preview-card-picture/index.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Router from 'next/router';
 import { useCardPicture } from '../../stores/useCardPicture';
 import { useRouter } from 'next/navigation';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function index() {
+  const [date, setDate] = useState<Date>(new Date());
   const router = useRouter();
-  const { pictureUrl } = useCardPicture();
+  const { pictureUrl, pictureDate, setPictureDate, pictureComment, setPictureComment } = useCardPicture();
   return (
     <>
       <h1>사진 확인하기</h1>
       <button onClick={() => router.back()}>뒤로가기</button>
       {pictureUrl && <img src={pictureUrl} style={{ width: 100, height: 100 }} />}
-      <button>편지쓰기</button>
+      <DatePicker fixedHeight selected={date} onChange={(date: Date) => setDate(date)} />
+      <input type="text" placeholder="사진에 대한 한마디" value={pictureComment} onChange={e => setPictureComment(e.target.value)} />
+      <button
+        onClick={() => {
+          setPictureDate(date);
+          console.log(pictureDate);
+          console.log(pictureComment);
+        }}
+      >
+        편지쓰기
+      </button>
     </>
   );
 }
--- frontend/stores/useCardPicture.ts
+++ frontend/stores/useCardPicture.ts
@@ -3,11 +3,23 @@ import create from 'zustand';
 interface CardPicture {
   pictureUrl: string | null;
   setPictureUrl: (select: string) => void;
+  pictureDate: Date | null;
+  setPictureDate: (select: Date) => void;
+  pictureComment: string;
+  setPictureComment: (select: string) => void;
 }
 
 export const useCardPicture = create<CardPicture>(set => ({
   pictureUrl: null,
+  pictureDate: null,
+  pictureComment: '',
   setPictureUrl: select => {
     set({ pictureUrl: select });
+  },
+  setPictureDate: select => {
+    set({ pictureDate: select });
+  },
+  setPictureComment: select => {
+    set({ pictureComment: select });
   },
 }));
