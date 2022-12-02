77e2fd4091478175225afd5d32b1c44ed9677b09
chchaeun <kce3615@naver.com>
2022-12-02 16:37:42 +0900

    feat: 클릭 시 편지 flip 기능 구현 (#13)

--- frontend/components/room/LetterView.tsx
+++ frontend/components/room/LetterView.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLetterView } from "../../stores/useLetterView";
+import ReactCardFlip from "react-card-flip";
 
 function LetterView() {
   const { letter, isLoading, error } = useLetterView();
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const flip = () => {
+    setIsFlipped((prev) => !prev);
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -12,7 +18,40 @@ function LetterView() {
     return <div>편지 불러오기에 실패했습니다.</div>;
   }
 
-  return <div>{letter?.id}</div>;
+  return (
+    <ReactCardFlip
+      isFlipped={isFlipped}
+      flipDirection="horizontal"
+      flipSpeedBackToFront={1}
+      flipSpeedFrontToBack={1}
+    >
+      <div
+        style={{
+          width: "250px",
+          height: "200px",
+        }}
+      >
+        <img
+          src={letter?.cardImg}
+          onClick={flip}
+          style={{
+            height: "200px",
+            width: "250px",
+          }}
+        />
+      </div>
+      <div
+        onClick={flip}
+        style={{
+          height: "200px",
+          width: "250px",
+          background: "aliceblue",
+        }}
+      >
+        {letter?.content} From. {letter?.writer}
+      </div>
+    </ReactCardFlip>
+  );
 }
 
 export default LetterView;
