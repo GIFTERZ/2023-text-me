16a6265ebd8fb5711040a9bf4ad6df27ba17c1de
seungki <sysh9498@gmail.com>
2022-12-03 01:10:39 +0900

    feat: 달력 한글화 및 포맷 설정(#16)

--- frontend/pages/preview-card-picture/index.tsx
+++ frontend/pages/preview-card-picture/index.tsx
@@ -4,23 +4,38 @@ import { useCardPicture } from '../../stores/useCardPicture';
 import { useRouter } from 'next/navigation';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { ko } from 'date-fns/locale';
 
 export default function index() {
   const [date, setDate] = useState<Date>(new Date());
   const router = useRouter();
   const { pictureUrl, pictureDate, setPictureDate, pictureComment, setPictureComment } = useCardPicture();
+  const dateToString = (date: Date) => {
+    return (
+      date.getFullYear() +
+      '년 ' +
+      (date.getMonth() + 1).toString().padStart(2, '0') +
+      '월 ' +
+      date.getDate().toString().padStart(2, '0') +
+      '일'
+    );
+  };
   return (
     <>
       <h1>사진 확인하기</h1>
       <button onClick={() => router.back()}>뒤로가기</button>
       {pictureUrl && <img src={pictureUrl} style={{ width: 100, height: 100 }} />}
-      <DatePicker fixedHeight selected={date} onChange={(date: Date) => setDate(date)} />
+      <DatePicker
+        locale={ko}
+        dateFormat="yyyy년 MM월 dd일"
+        fixedHeight
+        selected={pictureDate}
+        onChange={(date: Date) => setPictureDate(date)}
+      />
       <input type="text" placeholder="사진에 대한 한마디" value={pictureComment} onChange={e => setPictureComment(e.target.value)} />
       <button
         onClick={() => {
-          setPictureDate(date);
-          console.log(pictureDate);
-          console.log(pictureComment);
+          // 편지 작성 화면 이동
         }}
       >
         편지쓰기
