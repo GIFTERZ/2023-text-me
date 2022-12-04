a8b31d4814638d3715a61cd1b9de8697246c3304
chchaeun <kce3615@naver.com>
2022-12-04 17:01:48 +0900

    feat: Room 이름 추가 (#27)

--- frontend/pages/[uid]/index.tsx
+++ frontend/pages/[uid]/index.tsx
@@ -7,13 +7,14 @@ import SaveContainer from "../../components/room/SaveContainer";
 import LetterViewContainer from "../../components/room/LetterViewContainer";
 import { useRoomInfo } from "../../stores/useRoomInfo";
 import Background from "../../components/room/Background";
+import styled from "styled-components";
 
 function Room() {
   const { get } = useSearchParams();
   const pathname = usePathname();
   const userId = Number(get("uid"));
 
-  const { getRoomInfo } = useRoomInfo();
+  const { roomInfo, getRoomInfo } = useRoomInfo();
 
   useEffect(() => {
     getRoomInfo(userId);
@@ -21,17 +22,45 @@ function Room() {
 
   return (
     <Background>
-      <div>
-        <ShareContainer />
-        <SaveContainer />
-        {/* To do: 비로그인 구분 */}
-        <Link href="/">메인</Link>
-      </div>
-      <LettersContainer userId={userId} />
-      <Link href={`${pathname}/write`}>Text Username</Link>
-      <LetterViewContainer />
+      <Frame>
+        <Title>{roomInfo.ownerName}'s room</Title>
+        <div>
+          <ShareContainer />
+          <SaveContainer />
+          {/* To do: 비로그인 구분 */}
+          <Link href="/">메인</Link>
+        </div>
+        <LettersContainer userId={userId} />
+        <Link href={`${pathname}/write`}>Text Username</Link>
+        <LetterViewContainer />
+      </Frame>
     </Background>
   );
 }
 
 export default Room;
+
+const Frame = styled.div`
+  padding: 32px 24px;
+`;
+
+const Title = styled.h1`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 13px;
+
+  width: fit-content;
+  margin: 0;
+
+  background: #ffffff;
+  border-radius: 10px 10px 10px 0px;
+
+  font-weight: 700;
+  font-size: 17px;
+  line-height: 17px;
+
+  color: #0eca92;
+
+  box-shadow: 2px 2px 5px 1px rgba(62, 78, 82, 0.4);
+`;
