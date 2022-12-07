f5f20cc07b9998b371a4dec2ca9caf078b28203e
juy4556 <kws05050@naver.com>
2022-12-08 06:37:10 +0900

    refactor: 카드 업로드 (#94)

--- backend/text-me/src/main/java/gifterz/textme/domain/card/repository/CardRepository.java
+++ backend/text-me/src/main/java/gifterz/textme/domain/card/repository/CardRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
-
 public interface CardRepository extends JpaRepository<Card, Long> {
 }
