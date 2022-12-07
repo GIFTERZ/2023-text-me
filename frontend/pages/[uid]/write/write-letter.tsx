34bf177e0da6636f5f0c4a0b8d4dded2f0b02523
chchaeun <kce3615@naver.com>
2022-12-07 17:24:14 +0900

    fix: 쓰기 페이지 (#63)

--- frontend/pages/[uid]/write/write-letter.tsx
+++ frontend/pages/[uid]/write/write-letter.tsx
@@ -1,64 +1,98 @@
-import React, { useEffect } from 'react';
-import styled from 'styled-components';
-import { useLetterInfo } from '../../../stores/useLetterInfo';
-import { useRouter } from 'next/router';
-import { Frame } from '../../../styles/components/Frame';
-import { useForm } from 'react-hook-form';
-import { useRoomInfo } from '../../../stores/useRoomInfo';
-import { usePathname, useSearchParams } from 'next/navigation';
-import { useCardPicture } from '../../../stores/useCardPicture';
-import { LeftButton } from '../../../styles/components/Button';
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { useSendLetter } from "../../../stores/useSendLetter";
+import { useRouter } from "next/router";
+import { Frame } from "../../../styles/components/Frame";
+import { FieldErrors, useForm } from "react-hook-form";
+import { useRoomInfo } from "../../../stores/useRoomInfo";
+import { useSearchParams } from "next/navigation";
+import { RightButton } from "../../../styles/components/Button";
+import { useCardPicture } from "../../../stores/useCardPicture";
+
+type LetterForm = {
+  contents: string;
+  sender: string;
+};
 
 export default function index() {
-  interface imgurl {
-    imgurl: string;
-  }
-  const { register, handleSubmit } = useForm();
   const router = useRouter();
-  const { setLetterContents, setLetterSender } = useLetterInfo();
-  const sendData = (data: object) => {
-    console.log(data);
-    router.push('/:uid/write/send-complete');
-  };
+  const userId = useSearchParams().get("uid");
+  const { register, handleSubmit } = useForm<LetterForm>();
 
-  const { get } = useSearchParams();
-  const pathname = usePathname();
-  const userId = Number(get('uid'));
   const { pictureUrl } = useCardPicture();
   const { roomInfo, getRoomInfo } = useRoomInfo();
+
+  const { loading, error, sendLetter } = useSendLetter();
+
   useEffect(() => {
-    getRoomInfo(userId);
+    getRoomInfo(Number(userId));
   }, []);
+
+  const pushCompletedPage = () => {
+    router.push(`/${userId}/write/send-complete`);
+  };
+
+  const sendData = (data: LetterForm) => {
+    const body = {
+      content: data.contents,
+      writer: data.sender,
+      cardImg: pictureUrl,
+    };
+    sendLetter(body, pushCompletedPage);
+
+    if (error) {
+      alert("편지를 보내는 중 에러가 발생했습니다.");
+    }
+  };
+
+  const validateData = (error: FieldErrors<LetterForm>) => {
+    if (error.contents) {
+      alert(error.contents.message);
+      return;
+    }
+    if (error.sender) {
+      alert(error.sender.message);
+      return;
+    }
+  };
 
   return (
     <Frame>
       <Title>편지 쓰기</Title>
-      {/* <ToDiv>{roomInfo.ownerName && <p>To. {roomInfo.ownerName}</p>}</ToDiv> */}
-      <form onSubmit={handleSubmit(sendData)}>
+      <Form onSubmit={handleSubmit(sendData, validateData)}>
         <LetterContainer imgurl={pictureUrl} id="box">
-          <ToDiv>To. 아무개</ToDiv>
+          <ToDiv>To. {roomInfo?.ownerName}</ToDiv>
           <TextArea
-            id="contents"
-            {...register('contents', {
-              maxLength: 300,
+            maxLength={300}
+            {...register("contents", {
+              required: "편지를 입력해주세요.",
+              maxLength: {
+                value: 300,
+                message: "편지는 300자 이내여야 합니다.",
+              },
             })}
+            placeholder="편지를 입력해주세요."
           />
           <FromDiv>
             <p>From.</p>
             <FromInput
-              id="sender"
               type="text"
-              {...register('sender', {
-                required: '작성자를 입력해주세요.',
-                maxLength: 10,
+              placeholder="보내는 사람을 입력해주세요."
+              maxLength={10}
+              {...register("sender", {
+                required: "보내는 사람을 입력해주세요.",
+                maxLength: {
+                  value: 10,
+                  message: "보내는 사람 이름을 10자 이내로 입력해주세요.",
+                },
               })}
             />
           </FromDiv>
         </LetterContainer>
-        <LeftButton style={{ margin: '0 auto' }} type="submit">
+        <RightButton type="submit" disabled={loading}>
           보내기
-        </LeftButton>
-      </form>
+        </RightButton>
+      </Form>
     </Frame>
   );
 }
@@ -66,13 +100,12 @@ export default function index() {
 const LetterContainer = styled.div<{ imgurl: string }>`
   width: 100%;
   border-radius: 10px;
-  margin: 70px 0;
   padding: 20px;
   position: relative;
 
   ::before {
-    content: '';
-    background: url(${props => props.imgurl});
+    content: "";
+    background: url(${(props) => props.imgurl});
     background-size: cover;
     border-radius: 10px;
     opacity: 0.2;
@@ -84,15 +117,28 @@ const LetterContainer = styled.div<{ imgurl: string }>`
   }
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 70px;
+`;
+
 const TextArea = styled.textarea`
   width: 100%;
-  height: 400px;
+  height: 300px;
   align-items: center;
   background-color: transparent;
   border: none;
   padding: 10px;
   position: relative;
   z-index: 5;
+
+  color: #000000;
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 const ToDiv = styled.div`
@@ -116,6 +162,7 @@ const FromDiv = styled.div`
 `;
 
 const Title = styled.h1`
+  margin-bottom: 40px;
   font-style: normal;
   font-weight: 700;
   font-size: 20px;
@@ -131,4 +178,11 @@ const FromInput = styled.input`
   line-height: 24px;
   position: relative;
   z-index: 5;
+
+  border: none;
+  background: none;
+
+  &:focus {
+    outline: none;
+  }
 `;
