0d7c1d8a97454cc2fdc86e03f02df8ce9c7ca979
seungki <sysh9498@gmail.com>
2022-12-08 00:25:59 +0900

    fix: baseURL 수정(#62)

--- frontend/auth/api.ts
+++ frontend/auth/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { refresh, refreshError } from './refresh';
 
 const api = axios.create({
-  baseURL: 'https://t2xt.me/',
+  baseURL: 'https://t2xt.me',
 });
 
 api.interceptors.request.use(refresh, refreshError);
