a40f53409f3a26d803af0e2227b8dd27da4fa094
seungki <sysh9498@gmail.com>
2022-12-08 18:23:54 +0900

    fix: 변수명 수정(#81)

--- frontend/auth/api.ts
+++ frontend/auth/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { refresh, refreshError } from './refresh';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_PRODUCTION_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
 api.interceptors.request.use(refresh, refreshError);
