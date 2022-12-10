f9c01ddb6c7a9c7bddb119be58d30f8d053cd87d
chchaeun <kce3615@naver.com>
2022-12-10 17:02:40 +0900

    feat: 멤버관련 에러 핸들링 (#153)

--- frontend/pages/[uid]/index.tsx
+++ frontend/pages/[uid]/index.tsx
@@ -1,22 +1,23 @@
-import Link from 'next/link';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
-import LettersContainer from '../../components/room/LettersContainer';
-import LetterViewContainer from '../../components/room/LetterViewContainer';
-import { useRoomInfo } from '../../stores/useRoomInfo';
-import styled from 'styled-components';
-import ButtonsContainer from '../../components/room/ButtonsContainer';
-import { RightButton } from '../../styles/components/Button';
-import { useCaptureMode } from '../../stores/useCaptureMode';
-import SaveModal from '../../components/room/SaveModal';
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+import LettersContainer from "../../components/room/LettersContainer";
+import LetterViewContainer from "../../components/room/LetterViewContainer";
+import { useRoomInfo } from "../../stores/useRoomInfo";
+import styled from "styled-components";
+import ButtonsContainer from "../../components/room/ButtonsContainer";
+import { RightButton } from "../../styles/components/Button";
+import { useCaptureMode } from "../../stores/useCaptureMode";
+import SaveModal from "../../components/room/SaveModal";
+import ErrorContainer from "../../components/common/ErrorContainer";
 
 function Room() {
   const { get } = useSearchParams();
   const pathname = usePathname();
 
-  const userId = Number(get('uid'));
+  const userId = Number(get("uid"));
 
-  const { roomInfo, getRoomInfo } = useRoomInfo();
+  const { roomInfo, getRoomInfo, error } = useRoomInfo();
   const { isCaptureMode, toggleCaptureMode, modalOpen } = useCaptureMode();
 
   useEffect(() => {
@@ -24,6 +25,10 @@ function Room() {
       getRoomInfo(userId);
     }
   }, [userId]);
+
+  if (!roomInfo || error) {
+    return <ErrorContainer />;
+  }
 
   return (
     <>
@@ -79,7 +84,8 @@ const Title = styled.h1`
 
   z-index: 10;
 
-  box-shadow: 2px 2px 5px 1px rgba(62, 78, 82, 0.4), inset -2px -2px 3px rgba(106, 106, 106, 0.25),
+  box-shadow: 2px 2px 5px 1px rgba(62, 78, 82, 0.4),
+    inset -2px -2px 3px rgba(106, 106, 106, 0.25),
     inset 2px 2px 3px rgba(255, 255, 255, 0.5);
 
   @media ${({ theme }) => theme.device.small} {
@@ -102,7 +108,8 @@ const CTAButton = styled(RightButton)`
 
   padding: 13px 24px;
 
-  box-shadow: 2px 2px 5px 1px rgba(62, 78, 82, 0.4), inset -2px -2px 3px rgba(106, 106, 106, 0.25),
+  box-shadow: 2px 2px 5px 1px rgba(62, 78, 82, 0.4),
+    inset -2px -2px 3px rgba(106, 106, 106, 0.25),
     inset 2px 2px 3px rgba(255, 255, 255, 0.5);
 
   @media ${({ theme }) => theme.device.small} {
