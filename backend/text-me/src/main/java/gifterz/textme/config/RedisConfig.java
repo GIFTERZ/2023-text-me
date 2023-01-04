2bebec2f86ad006627a93f4e2e7c94b87ad48828
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-01-05 02:49:15 +0900

    feat: 레디스 구성 파일 추가(#252)

---
+++ backend/text-me/src/main/java/gifterz/textme/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package gifterz.textme.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.cache.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.cache.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean(name = "redisTemplate")
+    public StringRedisTemplate stringRedisTemplate() {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
+        return stringRedisTemplate;
+    }
+}
