bc87ecca17a4dd5205388c0e6129e920075a3303
juy4556 <wnstjrtmdgus@gmail.com>
2023-11-14 15:22:41 +0900

    feature: OauthMemberRepository 추가 및 findByOauthId 메서드 추가

---
+++ backend/text-me/src/main/java/gifterz/textme/domain/oauth/repository/OauthMemberRepository.java
@@ -0,0 +1,14 @@
+package gifterz.textme.domain.oauth.repository;
+
+import gifterz.textme.domain.oauth.entity.OauthId;
+import gifterz.textme.domain.oauth.entity.OauthMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+public interface OauthMemberRepository extends JpaRepository<OauthMember, Long> {
+
+    Optional<OauthMember> findByOauthId(OauthId oauthId);
+}
