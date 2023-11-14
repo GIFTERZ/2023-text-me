f5ebdcee97757e9980d501c304321b942e443e83
juy4556 <wnstjrtmdgus@gmail.com>
2023-11-14 15:02:20 +0900

    feature: oauthServerType 및 컨버터 추가

---
+++ backend/text-me/src/main/java/gifterz/textme/domain/oauth/util/OauthServerTypeConverter.java
@@ -0,0 +1,12 @@
+package gifterz.textme.domain.oauth.util;
+
+import gifterz.textme.domain.oauth.entity.OauthServerType;
+import org.springframework.core.convert.converter.Converter;
+
+public class OauthServerTypeConverter implements Converter<String, OauthServerType> {
+
+    @Override
+    public OauthServerType convert(String source) {
+        return OauthServerType.fromName(source);
+    }
+}
