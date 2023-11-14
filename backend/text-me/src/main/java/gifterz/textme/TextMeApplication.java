03b0e6dee04fcc9c2e2b64d53db84f86d43b8db5
juy4556 <wnstjrtmdgus@gmail.com>
2023-11-14 15:37:13 +0900

    refactor: @EnableConfigurationProperties에서 @ConfigurationPropertiesScan으로 변경

--- backend/text-me/src/main/java/gifterz/textme/TextMeApplication.java
+++ backend/text-me/src/main/java/gifterz/textme/TextMeApplication.java
@@ -1,14 +1,13 @@
 package gifterz.textme;
 
-import gifterz.textme.common.firebase.CacheProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableConfigurationProperties(value = {CacheProperties.class})
+@ConfigurationPropertiesScan
 public class TextMeApplication {
 
     public static void main(String[] args) {
