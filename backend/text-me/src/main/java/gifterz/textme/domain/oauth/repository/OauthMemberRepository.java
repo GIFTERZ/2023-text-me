f9c56e8ea972a9f690bc6d98999579dac126bafb
juy4556 <wnstjrtmdgus@gmail.com>
2023-12-22 15:58:06 +0900

    refactor: OauthMember 이메일 사용해 data접근 허용

--- backend/text-me/src/main/java/gifterz/textme/domain/oauth/repository/OauthMemberRepository.java
+++ backend/text-me/src/main/java/gifterz/textme/domain/oauth/repository/OauthMemberRepository.java
@@ -2,6 +2,7 @@ package gifterz.textme.domain.oauth.repository;
 
 import gifterz.textme.domain.oauth.entity.OauthId;
 import gifterz.textme.domain.oauth.entity.OauthMember;
+import gifterz.textme.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +12,7 @@ import java.util.Optional;
 public interface OauthMemberRepository extends JpaRepository<OauthMember, Long> {
 
     Optional<OauthMember> findByOauthId(OauthId oauthId);
+
+    Optional<OauthMember> findByEmail(String email);
+
 }
