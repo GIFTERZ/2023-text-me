897f81ed62f47651841e2db4fb3fba54fa24e1dd
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-01-02 22:27:34 +0900

    feat: 로그인 시 이메일 default 값

--- frontend/components/slow/EmailForm.tsx
+++ frontend/components/slow/EmailForm.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FieldValues } from "react-hook-form";
 import Form from "../../common/form/Form";
 import { BackgroundImage } from "../../pages/slow-letter";
 import { SelectContainer } from "../../pages/slow-letter/get-info";
+import { useMembers } from "../../stores/useMembers";
 import { useSlowLetterInfo } from "../../stores/useSlowLetterInfo";
 import EmailFormContext from "./EmailFormContext";
 
@@ -12,21 +13,32 @@ interface Props {
 
 function EmailForm({ complete }: Props) {
   const { setEmail } = useSlowLetterInfo();
+  const { member, getMember } = useMembers();
   const handleEmailSubmit = (data: FieldValues) => {
-    console.log(data.email);
     setEmail(data.email);
     complete();
   };
 
-  return (
-    <SelectContainer>
+  useEffect(() => {
+    getMember();
+  }, []);
+
+  const RenderForm = ({ email }: { email: string }) => {
+    return (
       <Form
         onSubmit={handleEmailSubmit}
         inputs={EmailFormContext.getContext()}
         buttonText={"확인"}
+        defaultValues={{ email }}
       >
         <h2>편지를 받을 이메일을 입력해주세요.</h2>
       </Form>
+    );
+  };
+
+  return (
+    <SelectContainer>
+      <RenderForm email={member ? member.email : ""} />
       <BackgroundImage src="/static/images/room-background.png" />
     </SelectContainer>
   );
