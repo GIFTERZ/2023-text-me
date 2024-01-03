cf4c8c9a944f9dcaab9755ec93f6e8ba5a5d840f
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-01-03 21:12:24 +0900

    fix: 헤더 z-index 에러

---
+++ frontend/components/slow/BackHeader.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import BackHeader from "../common/BackHeader";
+import Logo from "../common/Logo";
+interface Props {
+  onBackClick: Function;
+}
+function SlowBackHeader({ onBackClick }: Props) {
+  return (
+    <HeaderContainer>
+      <BackHeader onBackClick={() => onBackClick()}>
+        <Logo />
+      </BackHeader>
+    </HeaderContainer>
+  );
+}
+
+export default SlowBackHeader;
+
+const HeaderContainer = styled.div`
+  position: relative;
+  z-index: 2;
+  margin: 20px;
+`;
