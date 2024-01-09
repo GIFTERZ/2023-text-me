3c488f0321684d880394ffd0342f0f965a53704a
juy4556 <wnstjrtmdgus@gmail.com>
2024-01-09 15:18:37 +0900

    refactor: senderInfo 빌더 패턴, 정적 팩터리 메서드 적용

--- backend/text-me/src/main/java/gifterz/textme/domain/letter/dto/request/SenderInfo.java
+++ backend/text-me/src/main/java/gifterz/textme/domain/letter/dto/request/SenderInfo.java
@@ -1,11 +1,28 @@
 package gifterz.textme.domain.letter.dto.request;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Getter;
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public record SenderInfo(
-        String receiverName,
-        String phoneNumber
-) {
+@Builder
+@Getter
+public class SenderInfo {
+    private final String senderName;
+    private String imageUrl;
+    private String phoneNumber;
+    private String email;
+
+    public static SenderInfo of(String senderName, String phoneNumber) {
+        return SenderInfo.builder()
+                .senderName(senderName)
+                .phoneNumber(phoneNumber)
+                .build();
+    }
+
+    public static SenderInfo of(String email, String senderName, String imageUrl) {
+        return SenderInfo.builder()
+                .email(email)
+                .senderName(senderName)
+                .imageUrl(imageUrl)
+                .build();
+    }
 }
